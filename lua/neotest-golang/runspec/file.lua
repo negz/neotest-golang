b3cf998e49116dbc3b4159cc4f5efd35f315d505
Nic Cope <nicc@rk0n.org>
2025-04-18 17:57:43 -0700

    Add some logging to figure out why it won't select my package
    Signed-off-by: Nic Cope <nicc@rk0n.org>

--- lua/neotest-golang/runspec/file.lua
+++ lua/neotest-golang/runspec/file.lua
@@ -12,131 +12,141 @@ local M = {}
 --- @param strategy string
 --- @return neotest.RunSpec | neotest.RunSpec[] | nil
 function M.build(pos, tree, strategy)
-  if vim.tbl_isempty(tree:children()) then
-    return M.return_skipped(pos)
-  end
+	if vim.tbl_isempty(tree:children()) then
+		return M.return_skipped(pos)
+	end
 
-  local go_mod_filepath = lib.find.file_upwards("go.mod", pos.path)
-  if go_mod_filepath == nil then
-    logger.error(
-      "The selected file does not appear to be part of a valid Go module (no go.mod file found)."
-    )
-    return nil -- NOTE: logger.error will throw an error, but the LSP doesn't see it.
-  end
 
-  local go_mod_folderpath = vim.fn.fnamemodify(go_mod_filepath, ":h")
-  local pos_path_folderpath = vim.fn.fnamemodify(pos.path, ":h")
-  local golist_data, golist_error = lib.cmd.golist_data(pos_path_folderpath)
+	local go_mod_filepath = lib.find.file_upwards("go.mod", pos.path)
+	if go_mod_filepath == nil then
+		logger.error(
+			"The selected file does not appear to be part of a valid Go module (no go.mod file found)."
+		)
+		return nil -- NOTE: logger.error will throw an error, but the LSP doesn't see it.
+	end
 
-  local errors = nil
-  if golist_error ~= nil then
-    if errors == nil then
-      errors = {}
-    end
-    table.insert(errors, golist_error)
-  end
+	local go_mod_folderpath = vim.fn.fnamemodify(go_mod_filepath, ":h")
+	local pos_path_folderpath = vim.fn.fnamemodify(pos.path, ":h")
 
-  -- find the go package that corresponds to the pos.path
-  local package_name = "./..."
-  local pos_path_filename = vim.fn.fnamemodify(pos.path, ":t")
 
-  for _, golist_item in ipairs(golist_data) do
-    if golist_item.TestGoFiles ~= nil then
-      if
-        pos_path_folderpath == golist_item.Dir
-        and vim.tbl_contains(golist_item.TestGoFiles, pos_path_filename)
-      then
-        package_name = golist_item.ImportPath
-        break
-      end
-    end
-    if golist_item.XTestGoFiles ~= nil then
-      -- NOTE: XTestGoFiles are test files that are part of a [packagename]_test package.
-      if
-        pos_path_folderpath == golist_item.Dir
-        and vim.tbl_contains(golist_item.XTestGoFiles, pos_path_filename)
-      then
-        package_name = golist_item.ImportPath
-        break
-      end
-    end
-  end
+	local golist_data, golist_error = lib.cmd.golist_data(pos_path_folderpath)
 
-  -- find all top-level tests in pos.path
-  local test_cmd = nil
-  local json_filepath = nil
-  local regexp = M.get_regexp(pos.path)
-  if regexp ~= nil then
-    test_cmd, json_filepath =
-      lib.cmd.test_command_in_package_with_regexp(package_name, regexp)
-  else
-    -- fallback: run all tests in the package
-    test_cmd, json_filepath = lib.cmd.test_command_in_package(package_name)
-    -- NOTE: could also fall back to running on a per-test basis by using a bare return
-  end
+	local errors = nil
+	if golist_error ~= nil then
+		if errors == nil then
+			errors = {}
+		end
+		table.insert(errors, golist_error)
+	end
 
-  local runspec_strategy = nil
-  if strategy == "dap" then
-    dap.assert_dap_prerequisites()
-    runspec_strategy = dap.get_dap_config(pos_path_folderpath, regexp)
-    logger.debug("DAP strategy used: " .. vim.inspect(runspec_strategy))
-    dap.setup_debugging(pos_path_folderpath)
-  end
+	-- find the go package that corresponds to the pos.path
+	local package_name = "./..."
+	local pos_path_filename = vim.fn.fnamemodify(pos.path, ":t")
 
-  --- @type RunspecContext
-  local context = {
-    pos_id = pos.id,
-    golist_data = golist_data,
-    errors = errors,
-    test_output_json_filepath = json_filepath,
-  }
+	logger.debug("Handling file " .. pos_path_filename .. " in " .. pos_path_folderpath)
 
-  --- @type neotest.RunSpec
-  local run_spec = {
-    command = test_cmd,
-    cwd = pos_path_folderpath,
-    context = context,
-  }
+	for _, golist_item in ipairs(golist_data) do
+		if golist_item.TestGoFiles ~= nil then
+			if
+			    pos_path_folderpath == golist_item.Dir
+			    and vim.tbl_contains(golist_item.TestGoFiles, pos_path_filename)
+			then
+				package_name = golist_item.ImportPath
+				logger.debug("Matched " .. golist_item.ImportPath)
+				break
+			else
+				logger.debug("Didn't match " .. golist_item.ImportPath)
+			end
+		end
+		if golist_item.XTestGoFiles ~= nil then
+			-- NOTE: XTestGoFiles are test files that are part of a [packagename]_test package.
+			if
+			    pos_path_folderpath == golist_item.Dir
+			    and vim.tbl_contains(golist_item.XTestGoFiles, pos_path_filename)
+			then
+				package_name = golist_item.ImportPath
+				break
+			end
+		end
+	end
 
-  if runspec_strategy ~= nil then
-    run_spec.strategy = runspec_strategy
-    run_spec.context.is_dap_active = true
-  end
+	logger.debug("Final package name " .. package_name)
 
-  logger.debug({ "RunSpec:", run_spec })
-  return run_spec
+	-- find all top-level tests in pos.path
+	local test_cmd = nil
+	local json_filepath = nil
+	local regexp = M.get_regexp(pos.path)
+	if regexp ~= nil then
+		test_cmd, json_filepath =
+		    lib.cmd.test_command_in_package_with_regexp(package_name, regexp)
+	else
+		-- fallback: run all tests in the package
+		test_cmd, json_filepath = lib.cmd.test_command_in_package(package_name)
+		-- NOTE: could also fall back to running on a per-test basis by using a bare return
+	end
+
+	local runspec_strategy = nil
+	if strategy == "dap" then
+		dap.assert_dap_prerequisites()
+		runspec_strategy = dap.get_dap_config(pos_path_folderpath, regexp)
+		logger.debug("DAP strategy used: " .. vim.inspect(runspec_strategy))
+		dap.setup_debugging(pos_path_folderpath)
+	end
+
+	--- @type RunspecContext
+	local context = {
+		pos_id = pos.id,
+		golist_data = golist_data,
+		errors = errors,
+		test_output_json_filepath = json_filepath,
+	}
+
+	--- @type neotest.RunSpec
+	local run_spec = {
+		command = test_cmd,
+		cwd = pos_path_folderpath,
+		context = context,
+	}
+
+	if runspec_strategy ~= nil then
+		run_spec.strategy = runspec_strategy
+		run_spec.context.is_dap_active = true
+	end
+
+	logger.debug({ "RunSpec:", run_spec })
+	return run_spec
 end
 
 function M.return_skipped(pos)
-  --- @type RunspecContext
-  local context = {
-    pos_id = pos.id,
-    golist_data = {}, -- no golist output
-  }
+	--- @type RunspecContext
+	local context = {
+		pos_id = pos.id,
+		golist_data = {}, -- no golist output
+	}
 
-  --- Runspec designed for files that contain no tests.
-  --- @type neotest.RunSpec
-  local run_spec = {
-    command = { "echo", "No tests found in file" },
-    context = context,
-  }
-  return run_spec
+	--- Runspec designed for files that contain no tests.
+	--- @type neotest.RunSpec
+	local run_spec = {
+		command = { "echo", "No tests found in file" },
+		context = context,
+	}
+	return run_spec
 end
 
 function M.get_regexp(filepath)
-  local regexp = nil
-  local lines = {}
-  for line in io.lines(filepath) do
-    if line:match("func Test") then
-      line = line:gsub("func ", "")
-      line = line:gsub("%(.*", "")
-      table.insert(lines, lib.convert.to_gotest_regex_pattern(line))
-    end
-  end
-  if #lines > 0 then
-    regexp = "^(" .. table.concat(lines, "|") .. ")$"
-  end
-  return regexp
+	local regexp = nil
+	local lines = {}
+	for line in io.lines(filepath) do
+		if line:match("func Test") then
+			line = line:gsub("func ", "")
+			line = line:gsub("%(.*", "")
+			table.insert(lines, lib.convert.to_gotest_regex_pattern(line))
+		end
+	end
+	if #lines > 0 then
+		regexp = "^(" .. table.concat(lines, "|") .. ")$"
+	end
+	return regexp
 end
 
 return M
